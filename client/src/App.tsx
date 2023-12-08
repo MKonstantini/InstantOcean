7fdc56e7fe2c3698df4d7396a2bc923d743c44c2
Matan Konstantini <matanko35@gmail.com>
2023-12-08 22:31:42 +0200

    first foundations

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App container mt-5">
+      <header className='.bg-info-subtle p-5'>
+        <h1 className='text-primary display-1'>Instant Ocean</h1>
+        <h2 className='text-secondary'>From booking to sea in a heartbeat!</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
