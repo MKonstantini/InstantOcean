5de86824f024214ec588bc1efc5231124e925b0b
Matan Konstantini <matanko35@gmail.com>
2023-12-21 16:07:47 +0200

    progressed client-side

--- client/src/components/pages/Home.tsx
+++ client/src/components/pages/Home.tsx
@@ -1,8 +1,6 @@
 import { FunctionComponent } from "react";
 import { getAllCruises } from "../../services/dbFunctions";
 
-import logo from "../../assets/Gengar like a sir11.jpg"
-
 interface HomeProps {
 
 }
@@ -13,9 +11,7 @@ const Home: FunctionComponent<HomeProps> = () => {
     return (
         <section className="container my-5">
             <header className="">
-                <img src={logo} width="900px" height="200px" alt="" />
-
-                <video src="../../assets/cover_video.mp4" width="800" height="400" controls autoPlay loop muted />
+                <video src="/client/public/OceanVideo.mp4" width="800" height="400" controls autoPlay loop muted />
 
                 <h1 className='text-primary display-1'>Instant Ocean</h1>
                 <h2 className='text-secondary'>From booking to sea in a heartbeat!</h2>
